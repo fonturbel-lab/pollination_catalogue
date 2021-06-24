13b82484540399a7230471f0bbf287c6bc4117a5
Francisco E. Fontúrbel <fonturbel@gmail.com>
2021-06-24 17:11:49 -0400

    Updates

--- data_description.R
+++ data_description.R
@@ -8,44 +8,44 @@ attach(data)
 names(data)
 
 #Different papers used
-refs<-unique(data$dwc.reference)
+refs<-unique(data$reference)
 refs
 length(refs)
 
 ##PLANTS
 #Species covered
-speciesP<-unique(data$dwc.scientificNamePlants)
+speciesP<-unique(data$scientificNamePlants)
 speciesP
 length(speciesP)
 
 #Genera covered
-generaP<-unique(data$dwc.genusPlants)
+generaP<-unique(data$genusPlants)
 generaP
 length(generaP)
 
 #Families covered
-familiesP<-unique(data$dwc.familyPlants)
+familiesP<-unique(data$familyPlants)
 familiesP
 length(familiesP)
 
 ##ANIMALS
 #Species covered
-speciesA<-unique(data$dwc.scientificNameAnimals)
+speciesA<-unique(data$scientificNameAnimals)
 speciesA
 length(speciesA)
 
 #Genera covered
-generaA<-unique(data$dwc.genusAnimals)
+generaA<-unique(data$genusAnimals)
 generaA
 length(generaA)
 
 #Families covered
-familiesP<-unique(data$dwc.familyAnimals)
+familiesP<-unique(data$familyAnimals)
 familiesP
 length(familiesP)
 
 #Orders covered
-orders<-unique(data$dwc.orderAnimals)
+orders<-unique(data$orderAnimals)
 orders
 length(orders)
 
--- generate_maps.R
+++ generate_maps.R
@@ -7,10 +7,10 @@ attach(data)
 names(data)
 
 #Setting geographic coordinates space
-min(data$dwc.decimalLatitude, na.rm = TRUE)
-max(data$dwc.decimalLatitude, na.rm = TRUE)
-min(data$dwc.decimalLongitude, na.rm = TRUE)
-max(data$dwc.decimalLongitude, na.rm = TRUE)
+min(data$decimalLatitude, na.rm = TRUE)
+max(data$decimalLatitude, na.rm = TRUE)
+min(data$decimalLongitude, na.rm = TRUE)
+max(data$decimalLongitude, na.rm = TRUE)
 
 
 #Ploting a simple map, locating the sampling points in Chile
@@ -19,8 +19,8 @@ library(mapdata)
 library(maptools)
 library(mapproj)
 map(database= "world", regions = "Chile", col="darkgoldenrod1", fill=TRUE, projection="gilbert", orientation= c(90,0,270))
-lon <- (data$dwc.decimalLongitude)
-lat <- (data$dwc.decimalLatitude)
+lon <- (data$decimalLongitude)
+lat <- (data$decimalLatitude)
 coord <- mapproject(lon, lat, proj="gilbert", orientation=c(90, 0, 270))  #convert points to projected lat/long
 points(coord, pch=20, cex=1.2, col="red")  #plot converted points
 
